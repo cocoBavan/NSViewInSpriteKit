a4dfcde8fd89a1bdd825b8b3472e2d7e5dfc6daf
Bavan Balan <tbavananth@gmail.com>
2016-02-06 13:24:36 +1100

    Working Scenario

--- ViewHierachyTest/GameScene.swift
+++ ViewHierachyTest/GameScene.swift
@@ -11,11 +11,15 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
 		//Trying to add Custom View
-		let smallView = NSView(frame: NSRect(origin:CGPoint(x: 100,y: 100),   size: CGSize(width: 200, height: 200)))
-		let layer = CALayer()
-		layer.backgroundColor = CGColorCreateGenericRGB(1.0, 0.0, 0.0, 0.4)
-		smallView.layer = layer
-		view.superview!.addSubview(smallView)
+		let dispatchTime: dispatch_time_t = dispatch_time(DISPATCH_TIME_NOW, Int64(0.1 * Double(NSEC_PER_SEC)))
+		dispatch_after(dispatchTime, dispatch_get_main_queue(), {
+			let smallView = NSView(frame: NSRect(origin:CGPoint(x: 0,y: 0),   size: CGSize(width: 200, height: 200)))
+			let layer = CALayer()
+			layer.backgroundColor = CGColorCreateGenericRGB(1.0, 0.0, 0.0, 0.4)
+			smallView.layer = layer
+			self.view!.addSubview(smallView)
+			smallView.wantsLayer = true
+		})
     }
     
     override func mouseDown(theEvent: NSEvent) {
